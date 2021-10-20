3327abcfa60e3f0b9797f7fed58310a153f03c8d
NotTacosHub <smithclayton80@gmail.com>
2021-10-20 16:04:57 -0500

    added Portfolio Container

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 
+import PortfolioContainer from './portfolio-container';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Clayton Smith's Portfolio</h1>
-        <h2>Big Poppa's Projects</h2>
+        <PortfolioContainer />
+        <PortfolioContainer />
+        <PortfolioContainer />
         <div>
           {moment().format('MMMM Do YYYY, h:mm a')}
         </div>
