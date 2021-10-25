cb19c8c094513a632c091f57dd35e628b3b88c96
NotTacosHub <smithclayton80@gmail.com>
2021-10-25 13:01:27 -0500

    added links to the portfolio tabs

--- src/components/app.js
+++ src/components/app.js
@@ -11,6 +11,7 @@ import Home from "./pages/home"
 import About from "./pages/about"
 import Contact from "./pages/contact"
 import Blog from "./pages/blog"
+import PortfolioDetail from "./portfolio/portfolio-detail";
 
 export default class App extends Component {
   render() {
@@ -30,6 +31,7 @@ export default class App extends Component {
               <Route exact path="/about-me" component={About} />
               <Route exact path="/contact" component={Contact} />
               <Route exact path="/blog" component={Blog} />
+              <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
             </Switch>
           </div>
         </Router>
