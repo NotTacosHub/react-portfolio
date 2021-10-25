f7d58172b8c8ee89ec24929c12dd15a1e421e611
NotTacosHub <smithclayton80@gmail.com>
2021-10-25 12:04:10 -0500

    updated navigation and routing

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-import PortfolioContainer from './portfolio-container';
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home"
+import About from "./pages/about"
+import Contact from "./pages/contact"
+import Blog from "./pages/blog"
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Clayton Smith's Portfolio</h1>
-        <PortfolioContainer />
-        <PortfolioContainer />
-        <PortfolioContainer />
-        <div>
-          {moment().format('MMMM Do YYYY, h:mm a')}
-        </div>
+
+        <Router>
+          <div>
+
+            <h1>Clayton Smith's Portfolio</h1>
+
+            <div>{moment().format('MMMM Do YYYY, h:mm a')}</div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about-me" component={About} />
+              <Route exact path="/contact" component={Contact} />
+              <Route exact path="/blog" component={Blog} />
+            </Switch>
+          </div>
+        </Router>
+
       </div>
     );
   }
