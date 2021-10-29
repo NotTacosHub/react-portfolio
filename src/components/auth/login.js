96b7297f9f585d1746885accbaf6e6ab261b225b
NotTacosHub <smithclayton80@gmail.com>
2021-10-29 12:29:19 -0500

    added some of the log out function

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -7,8 +7,9 @@ export default class Login extends Component {
 
         this.state = {
             email: "",
-            password: ''
-        }
+            password: "",
+            errorText: ""
+        };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -16,30 +17,49 @@ export default class Login extends Component {
 
     handleChange(event) {
         this.setState({
-            [event.target.name]: event.target.value
-        })
+            [event.target.name]: event.target.value,
+            errorText: ""
+        });
     }
 
     handleSubmit(event) {
-        axios.post("https://api.devcamp.space/sessions",
-            {
-                client: {
-                    email: this.state.email,
-                    password: this.state.password
+        axios
+            .post(
+                "https://api.devcamp.space/sessions",
+                {
+                    client: {
+                        email: this.state.email,
+                        password: this.state.password
+                    }
+                },
+                { withCredentials: true }
+            )
+            .then(response => {
+                if (response.data.status === "created") {
+                    this.props.handleSuccessfulAuth();
+                } else {
+                    this.setState({
+                        errorText: "Wrong email or password"
+                    });
+                    this.props.handleUnsuccessfulAuth();
                 }
-            },
-            { withCredentials: true }
-        ).then(response => {
-            console.log("response", response);
-        });
+            })
+            .catch(error => {
+                this.setState({
+                    errorText: "An error occurred"
+                });
+                this.props.handleUnsuccessfulAuth();
+            });
 
         event.preventDefault();
     }
+
     render() {
         return (
             <div>
                 <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
 
+                <div>{this.state.errorText}</div>
 
                 <form onSubmit={this.handleSubmit}>
                     <input
@@ -61,9 +81,8 @@ export default class Login extends Component {
                     <div>
                         <button type="submit">Login</button>
                     </div>
-
                 </form>
             </div>
-        )
+        );
     }
 }
